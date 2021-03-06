bcbf8fcb51bb1b021ecc8646193e56a84398d85a
Uli Schlachter <psychon@znc.in>
2021-03-06 14:43:38 +0100

    I keep doing stupid mistakes

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -28,7 +28,7 @@ fi
 
 # Try running the tests in parallel so that the common case (tests pass) is
 # quick, but fall back to running them in sequence to make debugging easier.
-if false # ! ninja test
+if true # ! ninja test
 then
 	./complete-run.pl --parallel=1 || (cat latest/complete-run.log; false)
 fi
